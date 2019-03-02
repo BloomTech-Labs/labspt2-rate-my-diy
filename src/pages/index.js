2ca8d783db87273456d561faa4c8a7f4182cd96a
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-02 12:03:39 -0500

    initialized project and index page has title of the project

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+class landingPage extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hot Code</h1>
+            </>
+        )
+    }
+}
+
+export default landingPage;
