74185f28ccadb5516eb73763a9f884ff97128508
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-03-04 22:47:19 -0500

    added prisma and started defining schema and initial setup of server

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+const {GraphQLServer} = require('graphql-yoga');
+// 1
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `Hello World`
+  }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
