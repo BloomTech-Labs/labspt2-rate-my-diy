03f0718bb419468e1c0d4fc3ac6965b0d516202a
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-03-05 23:31:49 -0500

    defined resolvers file structure and modified index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 const {GraphQLServer} = require('graphql-yoga');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const User = require('./resolvers/User');
 
 const resolvers = {
-  Query: {
-    info: () => `Hello World`,
-    // getUsers: () => 
-  }
+  Mutatation,
+  Query,
+  User
 }
 
 const server = new GraphQLServer({
