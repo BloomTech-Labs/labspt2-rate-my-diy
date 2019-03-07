a610c6df313c449da1e341d8aab163a4b72fce0a
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-03-06 23:28:54 -0500

    added app.json back

--- client/front-end/src/components/footer/footer.js
+++ client/front-end/src/components/footer/footer.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class footer extends Component {
-  render() {
-    return (
-      <div className="footer">
+export default function footer() {
+  return (
+    <div className="footer">
       <nav className="bottomNav">
-      <a>Link One</a>
-      <a>Link Two</a>
-      <a>Link Three</a>
+        <a>Link One</a>
+        <a>Link Two</a>
+        <a>Link Three</a>
       </nav>
       <p> &copy 2018 Copyright <a href=''>Rate My DIY</a> </p>
-      </div>
-    )
-  }
+    </div>
+  )
 }
