963767f4bad148389a1cd34a597776da2caa529f
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-03-06 22:27:56 -0500

    added node-sass, google fonts

---
+++ client/front-end/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [`node-sass-plugin`,
+{
+  resolve: `gatsby-plugin-prefetch-google-fonts@1.4.0`,
+  options: {
+    family: `Roboto`,
+    subsets: `Open Sans`
+  }
+}]
+}
