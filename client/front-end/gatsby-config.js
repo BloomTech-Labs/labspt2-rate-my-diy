1f48567891751e630c3ec95853a0f142e1866512
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-03-06 22:48:37 -0500

    added some global styles, reset

--- client/front-end/gatsby-config.js
+++ client/front-end/gatsby-config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  plugins: [`node-sass-plugin`,
-{
-  resolve: `gatsby-plugin-prefetch-google-fonts@1.4.0`,
-  options: {
-    family: `Roboto`,
-    subsets: `Open Sans`
-  }
-}]
+  plugins: [`gatsby-plugin-sass`,
+]
 }
