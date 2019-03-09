4a8f973968fbdef0aedf9c69e9de9417d93136c2
Michael Trew <mtrew2015@gmail.com>
2019-03-09 13:54:43 -0500

    added three buttons, small, medium, large

---
+++ client/src/components/buttons/buttons.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const smallButton = () =>  {
+  return (
+    <button className="small"></button>
+  )
+}
+
+const mediumButton = () => {
+  return(
+    <button className="medium"></button>
+  )
+}
+
+const largeButton = () => {
+  return(
+    <button className="large"></button>
+  )
+}
+
+export {
+  smallButton,
+  mediumButton,
+  largeButton
+}
