da53279c0d28d23e56f4791f837145c75f9846a7
jbierly1 <james.bierly@gmail.com>
2019-03-09 13:09:11 -0500

    initial component placeholder from state set

--- client/src/components/searchbar/searchbar.js
+++ client/src/components/searchbar/searchbar.js
@@ -1,11 +1,34 @@
+//todo:
+//control searchbar component
+//make isLoggedIn state
+//make onClick for submit button
+//onClick should check isLoggedIn
+//if not logged in, display pop-up
+//pop-up should have dummy log in button
+// pop-up should have functional dismiss button
+//move on to o auth implementation
+//wire logged in state to larger app using react context
+
 import React from 'react';
 
-const SearchBar = () => {
-	return (
-		<div className="searchbar">
-			<input type="text" placeholder="&#x1f50d; search.." />
-		</div>
-	);
-};
+class SearchBar extends Component {
+	constructor() {
+		this.state = {
+			text: ' search'
+		};
+	}
+	render() {
+		return (
+			<div className="searchbar">
+				<input
+					type="text"
+					onChange="kj"
+					placeholder={`&#x1f50d; ${this.state.text}`}
+				/>
+				<button className="search">Search</button>
+			</div>
+		);
+	}
+}
 
 export default searchBar;
