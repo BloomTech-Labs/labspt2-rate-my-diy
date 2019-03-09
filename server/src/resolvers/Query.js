5a70871a9d53d8b1858940cfc2af68419b84805f
alasalle <angela.lasalle91@gmail.com>
2019-03-08 23:22:25 -0500

    worked on resolvers for reviews and stars

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -6,7 +6,22 @@ const getUsers = (parent, args, context) => {
   return context.prisma.users()
 }
 
+const GetReviews = (parent,args,context) => {
+  return context.prisma.reviews()
+}
+
+const getReviewById = (parents, args, context, info) => {
+  return context.prisma.review({where: {id: args.id}}, info)
+}
+
+const getStars = (parents, args, context) => {
+  return context.prisma.starses()
+}
+
 module.exports = {
   info,
   getUsers,
+  GetReviews,
+  getReviewById,
+  getStars
 }
