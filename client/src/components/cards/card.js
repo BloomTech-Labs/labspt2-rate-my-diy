b6c534da4ead76baeb9fe5b1b0e56d535092b749
Michael Trew <mtrew2015@gmail.com>
2019-03-09 14:50:36 -0500

    Cards Component Working On

---
+++ client/src/components/cards/card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+ function card() {
+  return (
+    <div className="card">
+    <p>Props.Image To Be Added</p>
+    <h2>StarsComponent To Be Added</h2>
+    <p>Props.user</p>
+    </div>
+  )
+}
+
+export default card
