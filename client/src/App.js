10e70ff571071dc419d2e390676c7f429ab4858b
Michael Trew <mtrew2015@gmail.com>
2019-03-09 14:27:31 -0500

    added buttons as classNames globally,should be able to use, btn small, btn medium and btn large anywhere within the app. exporting them as reusable components wasn't working as intended

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import Nav from './components/nav/nav'
-import Footer from './components/footer/footer'
+import Nav from './components/nav/nav';
+import Footer from './components/footer/footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <Nav/>
-      <Footer/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Nav />
+				<Footer />
+			
+			</div>
+		);
+	}
 }
 
 export default App;
