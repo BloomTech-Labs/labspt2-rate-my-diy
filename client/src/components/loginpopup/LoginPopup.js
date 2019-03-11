2ba3aa16e949b7b01b98e35756e4b67c096527f7
jbierly1 <james.bierly@gmail.com>
2019-03-11 13:42:33 -0400

    finished searchbar login opup basic outline... needs app state and o auth to complete

---
+++ client/src/components/loginpopup/LoginPopup.js
@@ -0,0 +1,21 @@
+import "./LoginPopup.scss";
+import React, { Component } from "react";
+
+class LoginPopup extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return this.props.show === true ? (
+      <div className="modal">
+        <div className="modal-content">
+          <h1>Login Popup Box </h1>
+          <button className="close button">Close</button>
+          <button className="login button">login button</button>
+        </div>
+      </div>
+    ) : null;
+  }
+}
+
+export default LoginPopup;
--- client/src/components/searchbar/searchbar.js
+++ client/src/components/searchbar/searchbar.js
@@ -1,44 +1,55 @@
 //todo:
-//make isLoggedIn state
-//make onClick for submit button
-//onClick should check isLoggedIn
-//if not logged in, display pop-up
-//pop-up should have dummy log in button
-// pop-up should have functional dismiss button
+
+// pop-up should have functional dismiss button-- dismiss button should set back popup state
+//functional dismiss button requires app state-- implement after Apollo Client/Redux/React Context etc.?
 //move on to o auth implementation
 //wire logged in state to larger app using react context
 
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import './searchbar.scss';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import LoginPopup from "../loginpopup/LoginPopup.js";
+import "./searchbar.scss";
 
 class SearchBar extends Component {
-	constructor() {
-		super();
-		this.state = {
-			text: ' search'
-		};
-	}
-	changeHandler = e => {
-		this.setState({ text: e.target.value });
-	};
-	render() {
-		return (
-			<div className="searchBar">
-				<span className="searchSpan">
-					<FontAwesomeIcon icon={faSearch} />
-					<input
-						type="text"
-						onChange={this.changeHandler}
-						placeholder={this.state.text}
-					/>
-				</span>
-				{/* will need to be replaced with Button component/styling when that file is completed */}
-				<button className="search">Search</button>
-			</div>
-		);
-	}
+  constructor() {
+    super();
+    this.state = {
+      text: " search",
+      isLoggedIn: false,
+      displayPopUp: false
+    };
+  }
+  changeHandler = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  clickHandler = e => {
+    e.preventDefault();
+    this.state.isLoggedIn
+      ? this.setState({ displayPopUp: false })
+      : this.setState({ displayPopUp: true });
+  };
+
+  render() {
+    return (
+      <div className="searchBar">
+        <span className="searchSpan">
+          <FontAwesomeIcon icon={faSearch} />
+          <input
+            type="text"
+            onChange={this.changeHandler}
+            placeholder={this.state.text}
+          />
+        </span>
+        {/* will need to be replaced with Button component/styling when that file is completed */}
+        <button className="search" onClick={this.clickHandler}>
+          Search
+        </button>
+        <LoginPopup show={this.state.displayPopUp} />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
