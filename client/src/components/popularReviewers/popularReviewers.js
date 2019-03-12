8b1107a1acc49f94b93b8d01272617dd1693fd6d
jbierly1 <james.bierly@gmail.com>
2019-03-11 23:45:57 -0400

    added popular reviewers card

---
+++ client/src/components/popularReviewers/popularReviewers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../cards/card.scss';
+
+function PopularCard() {
+	return (
+		<div className="card">
+			<p>Props.Image</p>
+			<p>Props.User</p>
+		</div>
+	);
+}
+
+export default PopularCard;
