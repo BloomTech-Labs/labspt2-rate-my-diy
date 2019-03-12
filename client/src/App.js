2ed1de2101bde7f531ab2dd07d5c6947ce5367a6
Michael Trew <mtrew2015@gmail.com>
2019-03-12 05:13:45 -0400

    put the example query into a separate folder called example for reference for the team, cleaned upp APP.js and removed whitespace.

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
 import Nav from './components/nav/nav';
 import Footer from './components/footer/footer';
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
+import ExampleQuery from './examples/exampleQuery';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
 				<Nav />
+				<ExampleQuery />
 				<Footer />
-
-				<Query
-					query={gql `
-						{
-							users(orderBy: username_ASC)
-  								{
-									id  
-    								username
-    								email
-  								}	
-						}
-					`}
-				>
-				{({ loading, error, data }) => {
-					if(loading) return <p>Loading...</p>
-					if(error) return <p>Error :(</p>
-					
-					return data.users.map(({id, username, email}) => (
-						<div key={id}>
-							<p>{username}</p>
-							<p>{email}</p>
-						</div>
-					));
-				}}
-				</Query>
 			</div>
 		);
 	}
