7abf9945424fe2a2b3281dc6c8fa81b481af7739
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-11 22:42:04 -0400

    implemented a standard query to show functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Nav from './components/nav/nav';
 import Footer from './components/footer/footer';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 class App extends Component {
 	render() {
@@ -8,7 +10,31 @@ class App extends Component {
 			<div className='App'>
 				<Nav />
 				<Footer />
-			
+
+				<Query
+					query={gql `
+						{
+							users(orderBy: username_ASC)
+  								{
+									id  
+    								username
+    								email
+  								}	
+						}
+					`}
+				>
+				{({ loading, error, data }) => {
+					if(loading) return <p>Loading...</p>
+					if(error) return <p>Error :(</p>
+					
+					return data.users.map(({id, username, email}) => (
+						<div key={id}>
+							<p>{username}</p>
+							<p>{email}</p>
+						</div>
+					));
+				}}
+				</Query>
 			</div>
 		);
 	}
