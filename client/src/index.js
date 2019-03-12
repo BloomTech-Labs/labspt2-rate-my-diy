53d53a401c6df671fc4b524b9b100375bcb337a0
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-11 22:23:08 -0400

    implemented Apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/_globals.scss'
 import App from './App';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "https://labspt-diy-1bdc9280b0.herokuapp.com/labspt2-diy/production"
+});
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
 
 
 
