100c09491bd245ab86f27fe14fbbf4e85d65544a
Michael Trew <mtrew2015@gmail.com>
2019-03-16 13:07:45 -0400

    added back default export

--- client/src/components/firebase/index.js
+++ client/src/components/firebase/index.js
@@ -1,5 +1,5 @@
 import FirebaseContext, {withFirebase} from './context'
 import Firebase from '../../firebase/firebase/firebase'
 // export {Firebase as default,FirebaseContext}
-
+export default firebase
 export {FirebaseContext, withFirebase}
