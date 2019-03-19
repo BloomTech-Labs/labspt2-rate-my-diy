c6d8c2f6067ece599030bf160c096aef4cbd1046
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-19 15:29:06 -0400

    created get routes for users

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
 
 const projectsDB = require('../db/helpers/projectsDb');
+const usersDB = require('../db/helpers/userDb');
 
 module.exports = server => {
    server.get('/api/v1/projects/:id', getProjects);
    server.get('/api/v1/projects', getProjects);
+   server.get('/api/v1/users/:id', getUsers);
+   server.get('/api/v1/users', getUsers);
 }
 
 const getProjects = (req, res) => {
@@ -16,5 +19,17 @@ const getProjects = (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ message: 'Error fetching projects', error: err });
+        });
+}
+
+const getUsers = (req, res) => {
+    const { id } = req.params;
+
+    usersDB.get(id)
+        .then(users => {
+            res.send(users);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error fetching users', error: err });
         });
 }
