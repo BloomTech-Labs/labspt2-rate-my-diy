ebad67287cf47d294734ca7b83ceac62fbce049a
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-18 23:44:32 -0400

    created project seeds

--- server/db/seeds/projects.js
+++ server/db/seeds/projects.js
@@ -1,13 +1,34 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {
+          name: "Hack the NSA",
+          category: "Security",
+          title_img: "https://www.kunm.org/sites/kunm/files/styles/x_large/public/201801/Anonymous.jpg",
+          title_blurb: "Let me show you how to hack the NSA",
+          rating: 3.5,
+          author_id: 3
+        },
+        {
+          name: "GraphQL Tutorial",
+          rating: 5,
+          title_img: "https://graphql.org/img/og_image.png",
+          title_blurb: "A GraphQL tutorial",
+          category: "tech",
+          author_id: 2
+        },
+        {
+          title_blurb: "Knitting sweaters for my nephews",
+          name: "Knitting sweaters",
+          rating: 4.2,
+          title_img: "https://3.bp.blogspot.com/-7Bstxi6NY4I/V40ZokQvXNI/AAAAAAAAFaE/60RbEP54uwg-2jFhywH1j9vuhyB3-AEFgCLcB/s1600/IMG_4479.JPG",
+          category: "knitting",
+          author_id: 1
+        }
       ]);
     });
 };
