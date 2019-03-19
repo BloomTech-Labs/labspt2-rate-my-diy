fa76935726619dff1cb70a0339fdccdb51a92cc0
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-19 15:22:59 -0400

    created 2 get routes to get projects

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,20 @@
 const axios = require('axios');
 
+const projectsDB = require('../db/helpers/projectsDb');
+
 module.exports = server => {
-   server.get('/', helloWorld);
+   server.get('/api/v1/projects/:id', getProjects);
+   server.get('/api/v1/projects', getProjects);
 }
 
-const helloWorld = (req, res) => {
-    res.json({ message: 'Hello world' });
+const getProjects = (req, res) => {
+    const { id } = req.params;
+
+    projectsDB.get(id)
+        .then(projects => {
+            res.send(projects);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error fetching projects', error: err });
+        });
 }
