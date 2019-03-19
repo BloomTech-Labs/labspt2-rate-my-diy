5898c7e094e50e7164929ebf47ac29dcd490bf84
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-18 23:57:51 -0400

    fix some bugs with my schema and seeds

--- server/db/migrations/20190318225405_projects.js
+++ server/db/migrations/20190318225405_projects.js
@@ -8,7 +8,6 @@ exports.up = function(knex, Promise) {
       table.string('title_image');
       table.string('title_blurb')
       table.float('rating').defaultTo(1);
-      table.timestamp('created_at').defaultTo(knex.fn.now());
       table.integer('author_id').unsigned().references('id').inTable('users');
       table.string('steps_array');
       table.string('review_array');
--- server/db/seeds/projects.js
+++ server/db/seeds/projects.js
@@ -6,26 +6,26 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('projects').insert([
         {
-          name: "Hack the NSA",
+          title: "Hack the NSA",
           category: "Security",
-          title_img: "https://www.kunm.org/sites/kunm/files/styles/x_large/public/201801/Anonymous.jpg",
+          title_image: "https://www.kunm.org/sites/kunm/files/styles/x_large/public/201801/Anonymous.jpg",
           title_blurb: "Let me show you how to hack the NSA",
           rating: 3.5,
           author_id: 3
         },
         {
-          name: "GraphQL Tutorial",
+          title: "GraphQL Tutorial",
           rating: 5,
-          title_img: "https://graphql.org/img/og_image.png",
+          title_image: "https://graphql.org/img/og_image.png",
           title_blurb: "A GraphQL tutorial",
           category: "tech",
           author_id: 2
         },
         {
           title_blurb: "Knitting sweaters for my nephews",
-          name: "Knitting sweaters",
+          title: "Knitting sweaters",
           rating: 4.2,
-          title_img: "https://3.bp.blogspot.com/-7Bstxi6NY4I/V40ZokQvXNI/AAAAAAAAFaE/60RbEP54uwg-2jFhywH1j9vuhyB3-AEFgCLcB/s1600/IMG_4479.JPG",
+          title_image: "https://3.bp.blogspot.com/-7Bstxi6NY4I/V40ZokQvXNI/AAAAAAAAFaE/60RbEP54uwg-2jFhywH1j9vuhyB3-AEFgCLcB/s1600/IMG_4479.JPG",
           category: "knitting",
           author_id: 1
         }
