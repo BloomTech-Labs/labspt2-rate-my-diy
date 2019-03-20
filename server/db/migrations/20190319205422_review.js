60a94f8ee4a9eccc5c07892099854eed6a9c6121
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-19 20:58:51 -0400

    created review schema

---
+++ server/db/migrations/20190319205422_review.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('reviews', table => {
+      table.increments();
+      table.string('title').notNullable();
+      table.string('category').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.string('title_image');
+      table.string('title_blurb')
+      table.float('rating').defaultTo(1);
+      table.integer('author_id').unsigned().references('id').inTable('users');
+      table.string('steps_array');
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('reviews');
+  };
