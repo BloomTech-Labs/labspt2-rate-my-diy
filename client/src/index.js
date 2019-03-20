ee166f5ea3c5cc3f05d73f18c8f32a38d4f4f1b2
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-19 22:48:41 -0400

    cleaned app

--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/_globals.scss';
 import App from './App';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-const client = new ApolloClient({
-	uri: 'https://secure-harbor-25045.herokuapp.com/'
-});
 
 ReactDOM.render(
 	<Router>
-		<ApolloProvider client={client}>
-			<App />
-		</ApolloProvider>
+		<App />
 	</Router>,
 	document.getElementById('root'),
 );
