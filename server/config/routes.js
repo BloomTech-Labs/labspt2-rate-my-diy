64ac207a8bbae99399c6e7d87e84caede9f6d7fa
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-19 22:21:21 -0400

    get routes for reviews are working

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,13 +2,17 @@ const axios = require('axios');
 
 const projectsDB = require('../db/helpers/projectsDb');
 const usersDB = require('../db/helpers/userDb');
+const reviewsDB = require('../db/helpers/reviewDb');
 
 module.exports = server => {
    server.get('/api/v1/projects/:id', getProjects);
    server.get('/api/v1/projects', getProjects);
-   server.get('/api/v1/projects/current/:date', getProjectsByDate)
+   server.get('/api/v1/projects/current/:date', getProjectsByDate);
    server.get('/api/v1/users/:id', getUsers);
    server.get('/api/v1/users', getUsers);
+   server.get('/api/v1/reviews/:id', getReviews);
+   server.get('/api/v1/reviews', getReviews);
+   server.get('/api/v1/reviews/current/:date', getReviewsByDate);
 }
 
 const getProjects = (req, res) => {
@@ -32,6 +36,30 @@ const getProjectsByDate = (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ message: 'Error fetching projects', error: err });
+        });
+}
+
+const getReviews = (req, res) => {
+    const { id } = req.params;
+
+    reviewsDB.get(id)
+        .then(reviews => {
+            res.send(reviews);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error fetching reviews', error: err });
+        });
+}
+
+const getReviewsByDate = (req, res) => {
+    const { date } = req.params;
+
+    reviewsDB.getByDate(date)
+        .then(reviews => {
+            res.send(reviews);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error fetching reviews', error: err });
         });
 }
 
