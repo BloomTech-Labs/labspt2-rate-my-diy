c2d0f9c112a60864dc624f6a357b4cf6f8af373b
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-03-21 11:28:49 -0400

    Adding class component for Twitter sign in.

---
+++ client/src/components/signIn/thirdPartyApi/twitter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { compose } from 'recompose'
+import { withRouter } from 'react-router-dom'
+import { withFirebase } from '../../../components/firebase/index'
+
+export default class SignInTwitterBase extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
