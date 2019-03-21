34716cdaf798852085b42c590ab0cfe76b95e8b5
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-21 10:26:43 -0400

    landing page is showing projects

--- client/src/components/featureProject/featureProject.js
+++ client/src/components/featureProject/featureProject.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../cards/card.scss';
 
 function FeaturedAndMakers(props) {
+	console.log(props.user)
 	return (
 		<div className="card">
 			<img src={props.image} alt='project' />
 			{props.type === 'featured' ? <h2>{props.title}</h2> : null}
-			<p>{props.user}</p>
+			<p>{props.user[0].username}</p>
 			<p>{props.stars}</p>
 		</div>
 	);
