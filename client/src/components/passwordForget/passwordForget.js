62fed3d9d8c00ac3ab3a6938fde6accec08caa31
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-03-20 23:14:19 -0400

    Adding google sign in function, and google provider object.

---
+++ client/src/components/passwordForget/passwordForget.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class passwordForget extends Component {
+  render() {
+    return (
+      <div>
+        <p>passwordForget</p>
+      </div>
+    );
+  }
+}
