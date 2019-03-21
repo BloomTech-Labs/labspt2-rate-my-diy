d4935c150614227e9698319b224d38dfaaf35248
Michael Trew <mtrew2015@gmail.com>
2019-03-20 22:43:48 -0400

    github sign in uploaded

--- client/src/components/signIn/thirdPartyApi/github.js
+++ client/src/components/signIn/thirdPartyApi/github.js
@@ -1,14 +1,52 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import * as ROUTES from '../../../constants/routes';
+import { compose } from 'recompose';
+import { withRouter } from 'react-router-dom';
+import { withFirebase } from '../../firebase';
 
-export default class signInGithub extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { error: null };
-  }
-  onSubmit = event => {
-    this.props.firebase.doSignInWithGithub();
-  };
-  render() {
-    return <div />;
-  }
+export default class SignInGithubBase extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { error: null };
+	}
+	onSubmit = (event) => {
+		this.props.firebase
+			.doSignInWithGithub()
+			.then((socialAuthUser) => {
+				return this.props.firebase.user(socialAuthUser.user.uid).set(
+					{
+						username: socialAuthUser.additionalUserInfo.profile.name,
+						email: socialAuthUser.additionalUserInfo.email,
+						roles: [],
+					},
+					{ merge: true },
+				);
+			})
+			.then(() => {
+				this.setState({ error: null });
+				this.props.history.push(ROUTES.HOME);
+			})
+			.catch((err) => {
+				if (err.code === ERROR_CODE_ACCOUNT_EXISTS) {
+					err.message = ERROR_MSG_ACCOUNT_EXISTS;
+				}
+				this.setState({ err });
+			});
+		event.preventDefault();
+	};
+	render() {
+		return (
+			<form onSubmit={this.onSubmit}>
+				<button type='submit'>Sign In with Github</button>
+				{error && <p>{error.message}</p>}
+			</form>
+		);
+	}
 }
+
+const SignInGithub = compose(
+  withRouter,
+  withFirebase,
+)(SignInGithubBase);
+
+export default SignInGithub
