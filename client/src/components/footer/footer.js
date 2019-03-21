146c37bf28f30eb3ad107882d2ce8f537b78384c
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-21 19:24:05 -0400

    added home and subscribe link to the footer for now

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -5,7 +5,9 @@ function Footer() {
   return (
     <div className="footer">
       <nav className="bottomNav">
+        <a href='/'>Home</a>
         <a href="/">About the Team</a>
+        <a href="/subscribe">Support Us</a>
       </nav>
       <p>2019 Copyright <a href='/'>Rate My DIY</a> </p>
     </div>
