ce619447b04d24d38dee32ada5b2bc68adcdea5a
alasalle <angela.lasalle91@gmail.com>
2019-03-21 00:56:10 -0400

    switched to javascript, working on localhost, will try to deploy

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const {path} = require('path')
+const path = require('path')
 const { GraphQLServer } = require('graphql-yoga') 
 const { makePrismaSchema, prismaObjectType } = require('nexus-prisma') 
 const { prisma } = require('./src/generated/prisma-client') 
--- server/src/stripe.js
+++ server/src/stripe.js
@@ -1,3 +1,7 @@
 const Stripe = require("stripe");
 
-export const stripe = Stripe("sk_test_Id16CLirynJIZtXzrnFdYxrL"); // stripe secret
+const stripe = Stripe("sk_test_Id16CLirynJIZtXzrnFdYxrL"); // stripe secret
+
+module.exports = {
+  stripe
+}
