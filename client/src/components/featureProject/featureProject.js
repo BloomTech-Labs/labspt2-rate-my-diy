89060e5cef73e8631c9a5f006fae982c45477430
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-23 09:56:39 -0400

    made change to feature project to fix the error

--- client/src/components/featureProject/featureProject.js
+++ client/src/components/featureProject/featureProject.js
@@ -6,7 +6,7 @@ function FeaturedAndMakers(props) {
     <div className="card">
       <img src={props.image} alt="project" />
       {props.type === "featured" ? <h2>{props.title}</h2> : null}
-      <p>{props.user[0].username}</p>
+      <p>{props.user.username}</p>
       <p>{props.stars}</p>
     </div>
   );
