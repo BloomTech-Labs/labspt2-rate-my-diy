b5ee305f0b7f88c1c036f77244afe5195c932e96
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-23 13:31:50 -0400

    added @ to username

--- client/src/components/Home/Featured/Featured.js
+++ client/src/components/Home/Featured/Featured.js
@@ -6,7 +6,7 @@ function Featured(props) {
     <div className="card" onClick={() => props.clickHandler(props.username)}>
       <img src={props.image} alt="project" />
       {props.type === "featured" ? <h2>{props.title}</h2> : null}
-      <p>{props.username}</p>
+      <p>@{props.username}</p>
       <p>{props.stars}</p>
     </div>
   );
