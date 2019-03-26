cc9d04f3e69c81776731d234b5b71c036a0a4a29
Michael Trew <mtrew2015@gmail.com>
2019-03-25 23:41:24 -0400

    working on Authorization HOC

--- client/src/components/Session/withAuthorization.js
+++ client/src/components/Session/withAuthorization.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {compose} from 'recompose';
+import {withFirebase} from '../Firebase/Exports';
+import * as ROUTES from '../../constants/routes';
 
 const withAuthorization = () => Component => {
   class WithAuthorization extends React.Component {
-    redner(){
+    componentDidMount(){
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        authUser => {
+          if (!condition(authUser)) {
+            this.props.history.push(ROUTES.SIGN_IN)
+          }
+        }
+      )
+    }
+    componentWillUnmount(){
+      this.listener();
+    }
+    render(){
       return <Component {...this.props}/>
     }
   }
