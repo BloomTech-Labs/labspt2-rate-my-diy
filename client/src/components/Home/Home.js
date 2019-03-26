b091cc55cd5d0c3e458f9b36391a1135fd9db897
TheGameFreak720 <cyanide6033@gmail.com>
2019-03-25 23:40:26 -0400

    makers are being sorted by rating

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -28,15 +28,13 @@ class Home extends Component {
 							
 		var year = currentTime.getFullYear()
 
-		const filteredData = [];
-
-		data.map(item => {
+		const filteredData = data.map(item => {
 			if (item.timestamp.slice(0, 4) == year && item.timestamp.slice(5, 7) == month) { 
-				filteredData.push(item);
+				return item;
 			}
 		});
 
-		return filteredData;
+		return filteredData.filter(function(e){return e});
 	}
 
 	render() {
@@ -110,10 +108,44 @@ class Home extends Component {
 						{({ loading, error, data }) => {
 							if (loading) return <p>Loading...</p>;
 							if (error) return <p>Error :(</p>;
+							
+							const currentMakers = data.users.map(user => {
+								const currentProject = this.filterByCurrentMonth(user.Projects);
+
+								if(currentProject.length === 0) {
+									return null;
+								}
+
+								const rating = currentProject.map(project => {
+									return project.rating;
+								});
+
+								///Checks for the mode average
+
+								let frequency = {};  // array of frequency.
+								let max = 0;  // holds the max frequency.
+								let average;   // holds the max frequency element.
+								for(let v in rating) {
+        							frequency[rating[v]]=(frequency[rating[v]] || 0)+1; // increment frequency.
+        							if(frequency[rating[v]] > max) { // is this frequency > max so far ?
+                							max = frequency[rating[v]];  // update max.
+                							average = rating[v];          // update result.
+									}
+								}
+
+								return {
+									"id": user.id,
+									"username": user.username,
+									"userProfileImage": user.userProfileImage,
+									"averageRating": average
+								}
+							}).filter(e => (e!==undefined) && (e!==null));
+
+							const sortedMakers = currentMakers.sort(function(a, b){return b.averageRating - a.averageRating}).slice(0, 8);
 
 							return (
 								<div className='card-container'>
-									{data.users.map(
+									{sortedMakers.map(
 										({ id, username, userProfileImage }) => (
 											<Featured
 												key={id}
