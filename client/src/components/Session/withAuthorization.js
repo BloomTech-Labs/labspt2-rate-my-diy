368b88e8218f62779eff43b2bb0af171d2a23fd5
Michael Trew <mtrew2015@gmail.com>
2019-03-25 23:53:55 -0400

    hoc complete

--- client/src/components/Session/withAuthorization.js
+++ client/src/components/Session/withAuthorization.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
-import {compose} from 'recompose';
-import {withFirebase} from '../Firebase/Exports';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { withFirebase } from '../Firebase/Exports';
 import * as ROUTES from '../../constants/routes';
 
-const withAuthorization = () => Component => {
-  class WithAuthorization extends React.Component {
-    componentDidMount(){
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
-        authUser => {
-          if (!condition(authUser)) {
-            this.props.history.push(ROUTES.SIGN_IN)
-          }
-        }
-      )
-    }
-    componentWillUnmount(){
-      this.listener();
-    }
-    render(){
-      return <Component {...this.props}/>
-    }
-  }
-  return compose(
-    withRouter, withFirebase,(WithAuthorization)
-  )
-}
+const withAuthorization = () => (Component) => {
+	class WithAuthorization extends React.Component {
+		componentDidMount() {
+			this.listener = this.props.firebase.auth.onAuthStateChanged((authUser) => {
+				if (!condition(authUser)) {
+					this.props.history.push(ROUTES.SIGN_IN);
+				}
+			});
+		}
+		componentWillUnmount() {
+			this.listener();
+		}
+		render() {
+			return <Component {...this.props} />;
+		}
+	}
+	return compose(withRouter, withFirebase, WithAuthorization);
+};
 export default withAuthorization;
