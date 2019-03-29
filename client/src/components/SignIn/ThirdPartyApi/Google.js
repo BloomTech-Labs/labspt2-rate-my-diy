403f907d64687f32268e52d0483fbcf4ba9a305b
Michael Trew <mtrew2015@gmail.com>
2019-03-29 06:56:50 -0400

    started working on Google Login and setting up logic for that function, verifiied that a new user does turn the newUser Flag to true

--- client/src/components/SignIn/ThirdPartyApi/Google.js
+++ client/src/components/SignIn/ThirdPartyApi/Google.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
-import { compose } from "recompose";
-import * as ROUTES from "../../../constants/routes";
-import { withRouter } from "react-router-dom";
-import { withFirebase } from "../../Firebase/Exports";
+import React, { Component } from 'react';
+import { compose } from 'recompose';
+import * as ROUTES from '../../../constants/routes';
+import { withRouter } from 'react-router-dom';
+import { withFirebase } from '../../Firebase/Exports';
 
-const ERROR_CODE_ACCOUNT_EXISTS =
-  "auth/account-exists-with-different-credential";
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/account-exists-with-different-credential';
 
 const ERROR_MSG_ACCOUNT_EXISTS = `
   An account with an E-Mail address to
@@ -15,55 +14,53 @@ const ERROR_MSG_ACCOUNT_EXISTS = `
 `;
 
 class SignInGoogleBase extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      error: null
-    };
-  }
+		this.state = {
+			error: null,
+		};
+	}
 
-  submitHandler = event => {
-    this.props.firebase
-      .doSignInWithGoogle()
-      .then(socialAuthUser => {
-        return this.props.firebase.user(socialAuthUser.user.uid).set(
-          {
-            username: socialAuthUser.user.displayName,
-            email: socialAuthUser.user.email,
-            roles: []
-          },
-          { merge: true }
-        );
-      })
-      .then(() => {
-        this.setState({ error: null });
-        this.props.history.push(ROUTES.HOME);
-      })
-      .catch(error => {
-        if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
-          error.message = ERROR_MSG_ACCOUNT_EXISTS;
-        }
-        this.setState({
-          error
-        });
-      });
-    event.preventDefault();
-  };
-  render() {
-    const { error } = this.state;
-    return (
-      <form onSubmit={this.submitHandler}>
-        <button type="submit">Sign In With Google</button>
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+	submitHandler = (event) => {
+		this.props.firebase
+			.doSignInWithGoogle()
+			.then((socialAuthUser) => {
+        console.log(socialAuthUser, 'return from Google')
+        var userBooleanValue = JSON.parse(socialAuthUser.additionalUserInfo.isNewUser);
+        console.log(userBooleanValue, 'newUser Indicator');
+				if (userBooleanValue) {
+					// do this if UserBooLeanValue === true
+				} else this.props.history.push(ROUTES.HOME);
+				return this.props.firebase.user(socialAuthUser.user.uid).set(
+					{
+						username: socialAuthUser.user.displayName,
+						email: socialAuthUser.user.email,
+					},
+					{ merge: true },
+				);
+			})
+			.catch((error) => {
+				if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+					error.message = ERROR_MSG_ACCOUNT_EXISTS;
+				}
+				this.setState({
+					error,
+				});
+			});
+		event.preventDefault();
+	};
+	render() {
+		const { error } = this.state;
+		return (
+			<form onSubmit={this.submitHandler}>
+				<button type='submit'>Sign In With Google</button>
+				{error && <p>{error.message}</p>}
+			</form>
+		);
+	}
 }
 
-const GoogleBase = compose(
-  withRouter,
-  withFirebase
-)(SignInGoogleBase);
+const GoogleBase = compose(withRouter, withFirebase)(SignInGoogleBase);
 
 export default GoogleBase;
