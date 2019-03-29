4cb6e543cc12ab2168ff1d48754ee11827fdf877
Michael Trew <mtrew2015@gmail.com>
2019-03-28 21:28:28 -0400

    format

--- client/src/components/SignIn/ThirdPartyApi/Github.js
+++ client/src/components/SignIn/ThirdPartyApi/Github.js
@@ -1,19 +1,10 @@
-import React, {
- Component
-} from "react";
-import * as ROUTES from "../../../constants/routes";
-import {
- compose
-} from "recompose";
-import {
- withRouter
-} from "react-router-dom";
-import {
- withFirebase
-} from "../../Firebase/Exports";
+import React, { Component } from 'react';
+import * as ROUTES from '../../../constants/routes';
+import { compose } from 'recompose';
+import { withRouter } from 'react-router-dom';
+import { withFirebase } from '../../Firebase/Exports';
 
-const ERROR_CODE_ACCOUNT_EXISTS =
- "auth/account-exists-with-different-credential";
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/account-exists-with-different-credential';
 
 const ERROR_MSG_ACCOUNT_EXISTS = `
   An account with an E-Mail address to
@@ -23,75 +14,67 @@ const ERROR_MSG_ACCOUNT_EXISTS = `
 `;
 
 class SignInGithubBase extends Component {
- constructor(props) {
-  super(props);
-  this.state = {
-   error: null
-  };
- }
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+		};
+	}
 
- setError = (errVal) => {
-  this.setState({
-   errVal
-  })
- }
- onSubmit = event => {
-  this.props.firebase
-   .doSignInWithGithub()
-   // console.log(this.props, 'home page props')
-   .then(socialAuthUser => {
-    // 1. Catch GH user object here, parse it for isNewUser project, ifNewUser === true, push to More Info page
-    console.log('response from gitHub:', socialAuthUser, )
-    var userBooleanValue = JSON.parse(socialAuthUser.additionalUserInfo.isNewUser)
-    console.log('userBooleanValue', userBooleanValue)
-    if (userBooleanValue) { 
-     /* userBooleanValue variable is set to the isNewUser key on the GH object,
+	setError = (errVal) => {
+		this.setState({
+			errVal,
+		});
+	};
+	onSubmit = (event) => {
+		this.props.firebase
+			.doSignInWithGithub()
+			// console.log(this.props, 'home page props')
+			.then((socialAuthUser) => {
+				// 1. Catch GH user object here, parse it for isNewUser project, ifNewUser === true, push to More Info page
+				console.log('response from gitHub:', socialAuthUser);
+				var userBooleanValue = JSON.parse(socialAuthUser.additionalUserInfo.isNewUser);
+				console.log('userBooleanValue', userBooleanValue);
+				if (userBooleanValue) {
+					/* userBooleanValue variable is set to the isNewUser key on the GH object,
       if that value === true, then push the route to more info page?
      */
-     // We could use if-else do-while or switch statement instead of while
-     this.props.history.push(ROUTES.MORE_INFO)
-     
-    }
-    else {
-     this.props.history.push(ROUTES.HOME)
-    }
-   })
-   .then((socialAuthUser) => {
-    // If user isn't new, userBooleanValue should evaluate to false on line 41, and then the code should skip to *here*,
-    // continuing the auth process.
-    return this.props.firebase.user(socialAuthUser.user.providerData.uid).set({
-     username: socialAuthUser.additionalUserInfo.profile.name,
-     email: socialAuthUser.additionalUserInfo.profile.email,
-     roles: []
-    });
-   })
-   .then((socialAuthUser) => {
-    return this.props.history.push(ROUTES.HOME);
-   })
-   .catch(err => {
-    if (err.code === ERROR_CODE_ACCOUNT_EXISTS) {
-     err.message = ERROR_MSG_ACCOUNT_EXISTS;
-    }
-    this.setError(err)
-   });
-  event.preventDefault();
- };
- render() {
-  const {
-   error
-  } = this.state;
-  return ( 
-  <form onSubmit = {this.onSubmit}>
-   <button type = "submit"> Sign In with Github </button> {
-    error && <p> {error.message} </p>} 
-     </form>
-   );
-  }
- }
+					// We could use if-else do-while or switch statement instead of while
+					this.props.history.push(ROUTES.MORE_INFO);
+				} else {
+					this.props.history.push(ROUTES.HOME);
+				}
+			})
+			.then((socialAuthUser) => {
+				// If user isn't new, userBooleanValue should evaluate to false on line 41, and then the code should skip to *here*,
+				// continuing the auth process.
+				return this.props.firebase.user(socialAuthUser.user.providerData.uid).set({
+					username: socialAuthUser.additionalUserInfo.profile.name,
+					email: socialAuthUser.additionalUserInfo.profile.email,
+					roles: [],
+				});
+			})
+			.then((socialAuthUser) => {
+				return this.props.history.push(ROUTES.HOME);
+			})
+			.catch((err) => {
+				if (err.code === ERROR_CODE_ACCOUNT_EXISTS) {
+					err.message = ERROR_MSG_ACCOUNT_EXISTS;
+				}
+				this.setError(err);
+			});
+		event.preventDefault();
+	};
+	render() {
+		const { error } = this.state;
+		return (
+			<form onSubmit={this.onSubmit}>
+				<button type='submit'> Sign In with Github </button> {error && <p> {error.message} </p>}
+			</form>
+		);
+	}
+}
 
- const SignInGithub = compose(
-  withRouter,
-  withFirebase
- )(SignInGithubBase);
+const SignInGithub = compose(withRouter, withFirebase)(SignInGithubBase);
 
- export default SignInGithub;
+export default SignInGithub;
