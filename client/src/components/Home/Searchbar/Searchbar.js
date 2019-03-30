48ec2a04519f5e306c7ec265c7767af2cae6c12a
alasalle <angela.lasalle91@gmail.com>
2019-03-29 20:00:12 -0400

    search will now let you know how any result type should be sorted

--- client/src/components/Home/Searchbar/Searchbar.js
+++ client/src/components/Home/Searchbar/Searchbar.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import LoginPopup from '../../LoginPopUp/LoginPopUp'
-import Fuse from 'fuse.js';
-import {Checkbox, CheckboxGroup} from 'react-checkbox-group';
+import LoginPopup from "../../LoginPopUp/LoginPopUp";
+import Fuse from "fuse.js";
+import { Checkbox, CheckboxGroup } from "react-checkbox-group";
 import "./Searchbar.scss";
 
 class SearchBar extends Component {
@@ -13,18 +13,23 @@ class SearchBar extends Component {
       text: "",
       isLoggedIn: false,
       displayPopUp: false,
-      options: ['project'],
+      options: ["project"],
+      userSort: "",
+      projectSort: "",
+      reviewSort: "",
       stars: 0,
       category: "",
       categoryDisabled: false,
-      starsDisabled: false
-
+      starsDisabled: false,
+      userSortDisabled: true,
+      projectSortDisabled: false,
+      reviewSortDisabled: true
     };
   }
 
   componentDidMount = () => {
-    console.log(this.props)
-  }
+    console.log(this.props);
+  };
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.userClicked !== null) {
@@ -42,82 +47,123 @@ class SearchBar extends Component {
       ? this.setState({ displayPopUp: false })
       : this.setState({ displayPopUp: true });
     // console.log(this.search(this.state.option));
-    let options = []
-    if (this.state.options.includes('user')) {
-      options.push('user')
+    let options = [];
+    if (this.state.options.includes("user")) {
+      options.push("user");
     }
-    if (this.state.options.includes('project')) {
-      options.push('project')
+    if (this.state.options.includes("project")) {
+      options.push("project");
     }
-    if (this.state.options.includes('review')) {
-      options.push('review')
+    if (this.state.options.includes("review")) {
+      options.push("review");
     }
-    if (this.state.options.includes('category')) {
-      options.push('category')
+    if (this.state.options.includes("category")) {
+      options.push("category");
     }
-    this.search(options)
+    this.search(options);
   };
 
   closePopUp = () => {
     this.setState({ displayPopUp: false });
   };
 
-  optionsChanged = (options) => {
-    if(!options.includes('project')) {
+  optionsChanged = options => {
+    if(options.includes('user')) {
+      this.setState({
+        userSortDisabled: false
+      })
+    }
+    if(!options.includes('user')){
+      this.setState({
+        userSortDisabled: true
+      })
+    }
+    if (!options.includes("project")) {
       this.setState({
         categoryDisabled: true,
-        starsDisabled: true})
+        starsDisabled: true,
+        projectSortDisabled: true
+      });
     }
-    if(options.includes('project')) {
+    if (options.includes("project")) {
       this.setState({
         categoryDisabled: false,
-        starsDisabled: false})
+        starsDisabled: false,
+        projectSortDisabled: false
+      });
+    }
+    if(!options.includes("review")) {
+      this.setState({
+        reviewSortDisabled: true
+      })
+    }
+    if(options.includes("review")) {
+      this.setState({
+        reviewSortDisabled: false
+      })
     }
     this.setState({
-      options: options
-    })
+      options: [...options]
+    });
+
+    console.log({state: this.state})
   };
 
   starChange = e => {
-    const stars = parseInt(e.target.value)
+    const stars = parseInt(e.target.value);
     this.setState({
       stars: stars
-    })
-  }
+    });
+  };
 
   categoryChange = e => {
     this.setState({
       category: e.target.value,
-      options: [...this.state.options, 'category']
-    })
+      options: [...this.state.options, "category"]
+    });
+  };
+
+  sortChange = option => {
+    if(this.state.options.includes('user')) {
+      this.setState({
+        userSort: option[0]
+      })
+    }
+    if(this.state.options.includes('project')) {
+      console.log({option: [...option]})
+      this.setState({
+        projectSort: option[0]
+      })
+    }
+    if(this.state.options.includes('review')) {
+      this.setState({
+        reviewSort: option[0]
+      })
+    }
+    
+    console.log({stateAfterSort: this.state, sortOption: option[0]})
   }
 
-  search = (option) => {
+  search = option => {
     let options = {
       keys: []
+    };
+
+    if (option.includes("user")) {
+      options.keys.push("username");
     }
 
-    if(option.includes('user')) {
-      options.keys.push('username')
-
-    } 
-    
-    if (option.includes('category')) {
-      options.keys.push('category')
-
+    if (option.includes("category")) {
+      options.keys.push("category");
     }
-    if (option.includes('review')) {
-      options.keys.push('name')
+    if (option.includes("review")) {
+      options.keys.push("name");
     }
-    // if (option.includes('stars')) {
-    //   options.keys.push('Projects.rating')
-    // }
-    if (option.includes('project')) {
-      options.keys.push('name')
-    } 
-    
-    else {
-      options.keys.push('name')
+
+    if (option.includes("project")) {
+      options.keys.push("name");
+    } else {
+      options.keys.push("name");
     }
     const usersFuse = new Fuse(this.props.users, options);
     const userSearch = usersFuse.search(this.state.text);
@@ -125,45 +171,71 @@ class SearchBar extends Component {
     const projectSearch = projectsFuse.search(this.state.text);
     const reviewsFuse = new Fuse(this.props.reviews, options);
     const reviewSearch = reviewsFuse.search(this.state.text);
-    const starsSearch = projectSearch.filter(project => project.rating >= this.state.stars);
+    const starsSearch = this.props.projects.filter(
+      project => project.rating >= this.state.stars
+    );
     const categorySearch = projectsFuse.search(this.state.category);
     // return fuse.search(this.state.tex);
-    console.log({state: this.state})
-    console.log({searchprops: this.props})
-    console.log({users: userSearch, projects: projectSearch, reviews: reviewSearch, stars: starsSearch, projectsByCategory: categorySearch});
-  }
-  
-
+    console.log({ state: this.state });
+    console.log({ searchprops: this.props });
+    console.log({
+      users: userSearch,
+      projects: projectSearch,
+      reviews: reviewSearch,
+      stars: starsSearch,
+      projectsByCategory: categorySearch
+    });
+  };
 
   render() {
-    let categories = this.props.projects.map(project => project.category)
-    let filteredCategories = [...new Set(categories)]
+    let categories = this.props.projects.map(project => project.category);
+    let filteredCategories = [...new Set(categories)];
     return (
       <div>
-        <form onSubmit={this.handleSubmit} className="searchBar">
-        <div>Search By:</div>
-        <CheckboxGroup
-        checkboxDepth={2} // This is needed to optimize the checkbox group
-        name="options"
-        value={this.state.options}
-        onChange={this.optionsChanged}>
- 
-        <label><Checkbox value="user"/> User</label>
-        <label><Checkbox value="project"/> Project</label>
-        <label><Checkbox value="review"/> Review</label>
-      </CheckboxGroup>
-      <select name="stars" onChange={this.starChange} value={this.state.stars} disabled={this.state.starsDisabled}>
-        <option value="1">1+ Stars</option>
-        <option value="2">2+ Stars</option>
-        <option value="3">3+ Stars</option>
-        <option value="4">4+ Stars</option>
-        <option value="5">5+ Stars</option>
-      </select>
-      <select name = "category" onChange={this.categoryChange} value={this.state.category} disabled={this.state.categoryDisabled}>
-        {filteredCategories.map(category => {
-          return <option value={category} key={category}>{`${category}`}</option>
-        })}
-      </select>
+        <form onSubmit={this.handleSubmit} >
+          <div>Search By:</div>
+          <CheckboxGroup
+            checkboxDepth={2} // This is needed to optimize the checkbox group
+            name="options"
+            value={this.state.options}
+            onChange={this.optionsChanged}
+          >
+            <label>
+              <Checkbox value="user" /> User
+            </label>
+            <label>
+              <Checkbox value="project" /> Project
+            </label>
+            <label>
+              <Checkbox value="review" /> Review
+            </label>
+          </CheckboxGroup>
+          <select
+            name="stars"
+            onChange={this.starChange}
+            value={this.state.stars}
+            disabled={this.state.starsDisabled}
+          >
+            <option>Stars</option>
+            <option value="1">1+ Stars</option>
+            <option value="2">2+ Stars</option>
+            <option value="3">3+ Stars</option>
+            <option value="4">4+ Stars</option>
+            <option value="5">5+ Stars</option>
+          </select>
+          <select
+            name="category"
+            onChange={this.categoryChange}
+            value={this.state.category}
+            disabled={this.state.categoryDisabled}
+          >  
+            <option value="">Categories</option>
+            {filteredCategories.map(category => {
+              return (
+                <option value={category} key={category}>{`${category}`}</option>
+              );
+            })}
+          </select>
           <div className="searchSpan">
             <FontAwesomeIcon icon={faSearch} className="icon" />
             <input
@@ -173,6 +245,62 @@ class SearchBar extends Component {
             />
           </div>
           <input className="searchButton" type="submit" value="Search" />
+          
+          
+          <div>Sort Users:</div>
+            <CheckboxGroup
+            checkboxDepth={2}
+            name="userSort"
+            value={this.state.userSort}
+            onChange={this.sortChange}
+            >
+            <label>
+              <Checkbox value="user_alphabetical" disabled={this.state.userSortDisabled}/> alphabetical
+            </label>
+            <label>
+              <Checkbox value="user_alphabetical_reverse" disabled={this.state.userSortDisabled}/> reverse alphabetical
+            </label>
+            </CheckboxGroup>
+            <div>Sort Projects:</div>
+            <CheckboxGroup
+            checkboxDepth={2}
+            name="projectSort"
+            value={this.state.projectSort}
+            onChange={this.sortChange}
+            >
+            <label>
+              <Checkbox value="project_alphabetical" disabled={this.state.projectSortDisabled}/> alphabetical
+            </label>
+            <label>
+              <Checkbox value="project_alphabetical_reverse" disabled={this.state.projectSortDisabled}/> reverse alphabetical
+            </label>
+            <label>
+              <Checkbox value="project_highest_rated" disabled={this.state.projectSortDisabled}/> highest rated
+            </label>
+            <label>
+              <Checkbox value="project_lowest_rated" disabled={this.state.projectSortDisabled}/> lowest rated
+            </label>
+            </CheckboxGroup>
+            <div>Sort Reviews:</div>
+            <CheckboxGroup
+            checkboxDepth={2}
+            name="reviewSort"
+            value={this.state.reviewSort}
+            onChange={this.sortChanged}
+            >
+            <label>
+              <Checkbox value="review_alphabetical" disabled={this.state.reviewSortDisabled}/> alphabetical
+            </label>
+            <label>
+              <Checkbox value="review_reverse_alphabetical" disabled={this.state.reviewSortDisabled}/> reverse alphabetical
+            </label>
+            <label>
+              <Checkbox value="review_newest" disabled={this.state.reviewSortDisabled}/> newest
+            </label>
+            <label>
+              <Checkbox value="review_oldest" disabled={this.state.reviewSortDisabled}/> oldest
+            </label>
+            </CheckboxGroup>
         </form>
 
         <LoginPopup
@@ -181,8 +309,7 @@ class SearchBar extends Component {
         />
       </div>
     );
-    }
   }
-
+}
 
 export default SearchBar;
