63000745ad9a13ef923b3fc12781714f6a399cd2
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-04-05 15:19:26 -0400

    Adding subscription component to render form.

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -90,6 +90,7 @@ class SignUpFormBase extends Component {
 		const { username, email, passwordOne, passwordTwo, error } = this.state;
 		const isInvalid = passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === '';
 		return (
+   <>
 			<Mutation mutation={firebaseSignUp}>
 				{(signUpMutation, { data }) => {
 					return (
@@ -171,6 +172,15 @@ class SignUpFormBase extends Component {
 					);
 				}}
 			</Mutation>
+   <Subscription subscription={sendEmail}>
+    {(sendEmail, { data }) => {
+     return (
+      <>
+      </>
+     )
+    }}
+   </Subscription>
+   </>
 		);
 	}
 }
