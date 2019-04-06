15a7b95e48cb72b5914e5b0ccd21d8e2ced2957c
Michael Trew <mtrew2015@gmail.com>
2019-04-06 12:39:47 -0400

    fixed a cpl errors..

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -89,7 +89,7 @@ class SignUpFormBase extends Component {
 		const { username, email, passwordOne, passwordTwo, error } = this.state;
 		const isInvalid = passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === '';
 		return (
-   <>
+   <React.Fragment>
 			<Mutation mutation={firebaseSignUp}>
 				{(signUpMutation, { data }) => {
 					return (
@@ -171,15 +171,15 @@ class SignUpFormBase extends Component {
 					);
 				}}
 			</Mutation>
-   <Subscription subscription={sendEmail}>
+        <Subscription subscription={EMAIL_SUBSCRIPTION}>
     {(sendEmail, { data }) => {
      return (
-      <>
-      </>
+      <React.Fragment>
+      </React.Fragment>
      )
     }}
    </Subscription>
-   </>
+   </React.Fragment>
 		);
 	}
 }
