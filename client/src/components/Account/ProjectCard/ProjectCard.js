07193f195063faa7f90cd0bf3e9def88aee7a105
alasalle <angela.lasalle91@gmail.com>
2019-04-08 23:32:23 -0400

    fleshed out project view card reformatted steps/changed project steps datamodel

--- client/src/components/Account/ProjectCard/ProjectCard.js
+++ client/src/components/Account/ProjectCard/ProjectCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProjectCard = ({ project }) => {
-  let steps = project.steps.split('1' || '2' || '3' || '4');
+  let steps = JSON.parse(project.steps);
 
   return (
     <div>
@@ -10,6 +10,13 @@ const ProjectCard = ({ project }) => {
       <div>{`${project.rating}`}</div>
       <div>{`${project.timestamp}`}</div>
       <img src={`${project.titleImg}`} alt="project" />
+      {steps.map((step) => {
+        if (step.type === 'img') {
+          return <img key={step.body} src={step.body} />;
+        } else {
+          return <div key={step.body}>{`${step.body}`}</div>;
+        }
+      })}
     </div>
   );
 };
