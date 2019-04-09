3f724005dec3f62617eba05bcc2027a50aac2ea6
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-04-08 23:38:37 -0400

    Setting up Query.

--- client/src/components/ReviewModal/ReviewModal.js
+++ client/src/components/ReviewModal/ReviewModal.js
@@ -1,6 +1,24 @@
 import './ReviewModal.scss';
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo';
+import React, {Component} from 'react';
 
-import React, { Component } from 'react'
+const GET_REVIEWS = gql `
+{
+    reviews {
+      id
+      name
+      rKey
+      text
+      timestamp
+      thumbsUp
+      LikedBy
+      thumbsDown
+      DislikedBy
+      Author
+      ProjectReviewed
+    }
+}`;
 
  class ReviewModal extends Component {
   constructor(props) {
@@ -10,14 +28,48 @@ import React, { Component } from 'react'
        
     }
   }
-  
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+
+  onReviewSelected = review => {
+   
   }
+  
+// render() {
+//  return (
+//   <div>
+//    const Reviews = ({ Review }) => ({
+//     <Query query={GET_REVIEWS}>
+//      {({ loading, error, data }) => {
+//      if (loading) return "Loading...";
+//      if (error) return `Error! ${error.message}`;
+//      console.log(data);
+
+//      return (
+
+//      <div>
+
+//      </div>
+//     );
+//      }}
+//     </Query>
+//    }) 
+//   </div>
+//  )}
+
+render() {
+
+const Reviews = ({Review}) => (
+  <Query 
+  query={GET_REVIEWS}>
+  </Query>
+)
+
+return (
+ <div>
+  <Reviews />
+ </div>
+)
+}
+
 }
 
 export default ReviewModal
