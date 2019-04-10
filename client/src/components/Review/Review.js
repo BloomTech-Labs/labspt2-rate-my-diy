b543a967ee7cdbcf8d7e1f64f1e5b2d7f6caad5f
Michael Trew <mtrew2015@gmail.com>
2019-04-10 00:07:58 -0400

    working on review

--- client/src/components/Review/Review.js
+++ client/src/components/Review/Review.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { getReviews } from '../../query/query.js';
-import {Query} from 'react-apollo';
-import gql from 'graphql-tag'
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 class Review extends Component {
 	constructor(props) {
 		super(props);
@@ -9,50 +9,50 @@ class Review extends Component {
 		this.state = {};
 	}
 
-  render() {
-    const Reviews = ({ Review }) => (
-      <Query query={getReviews}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :(</p>;
-          
-            const id = this.props.match.params;
-            console.log(id);
+	render() {
+		const Reviews = ({ Review }) => (
+			<Query query={getReviews}>
+				{({ loading, error, data }) => {
+					if (loading) return <p>Loading...</p>;
+					if (error) return <p>Error :(</p>;
 
-            const currentReview = data.reviews.filter(review => {
-              return review.id === id;
-            })
-            console.log(currentReview, 'current Review')
-          return (
-            <React.Fragment>
-              <h1 className='headerReview'>Reviews</h1>
-              <div className='card-container'>
-                {data.reviews.map(({ id, text, name, timestamp, Author, ProjectReviewed }) => (
-                  <div key={id} className={'review-card'} to={`/reviews/${id}`}>
-                    <div>
-                      <p className='review-name'>Reviewer: {`${Author.username} `}</p>
-                      <h2 className='project-name'>Project: {`${ProjectReviewed.name}`}</h2>
-                      <img alt={'project'} className='review-img' src={`${ProjectReviewed.titleImg}`} />
+					const id = this.props.match.params;
+					console.log(id, 'id');
+					console.log(data.reviews, 'reviews');
+					console.log(data.reviews, 'reviews');
+					const currentReview = data.reviews.find((review) => {
+						return review.id[0] === id[0];
+					});
+					console.log(currentReview, 'current Review');
+					return (
+						<React.Fragment>
+							<h1 className='headerReview'>Reviews</h1>
+							<div className='card-container'>
+								{data.reviews.map(({ id, text, name, timestamp, Author, ProjectReviewed }) => (
+									<div key={id} className={'review-card'} to={`/reviews/${id}`}>
+										<div>
+											<p className='review-name'>Reviewer: {`${Author.username} `}</p>
+											<h2 className='project-name'>Project: {`${ProjectReviewed.name}`}</h2>
+											<img alt={'project'} className='review-img' src={`${ProjectReviewed.titleImg}`} />
 
-                      <p className='review-text'>Text: {`${text}`}</p>
-                      <p className='project-extra'>{`${name}`}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </React.Fragment>
-          );
-        }}
-      </Query>
-    );
+											<p className='review-text'>Text: {`${text}`}</p>
+											<p className='project-extra'>{`${name}`}</p>
+										</div>
+									</div>
+								))}
+							</div>
+						</React.Fragment>
+					);
+				}}
+			</Query>
+		);
 
-    return (
-      <div>
-        <Reviews />
-      </div>
-    );
-  }
+		return (
+			<div>
+				<Reviews />
+			</div>
+		);
+	}
 }
-
 
 export default Review;
