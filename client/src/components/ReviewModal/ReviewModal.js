50ea9b5ab45d4799646bb6d25f62cab75313076a
Michael Trew <mtrew2015@gmail.com>
2019-04-09 22:11:10 -0400

    responsiveness

--- client/src/components/ReviewModal/ReviewModal.js
+++ client/src/components/ReviewModal/ReviewModal.js
@@ -1,101 +1,77 @@
 import './ReviewModal.scss';
 import { Query } from 'react-apollo';
-import React, {Component} from 'react';
-import { getReviews } from '../../query/query'
+import React, { Component } from 'react';
+import { getReviews } from '../../query/query';
 
+class ReviewModal extends Component {
+	constructor(props) {
+		super(props);
 
+		this.state = {};
+	}
 
- class ReviewModal extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       
-    }
-  }
+	onReviewSelected = (review) => {};
 
-  onReviewSelected = review => {
-   
-  }
-  
-// render() {
-//  return (
-//   <div>
-//    const Reviews = ({ Review }) => ({
-//     <Query query={GET_REVIEWS}>
-//      {({ loading, error, data }) => {
-//      if (loading) return "Loading...";
-//      if (error) return `Error! ${error.message}`;
-//      console.log(data);
+	// render() {
+	//  return (
+	//   <div>
+	//    const Reviews = ({ Review }) => ({
+	//     <Query query={GET_REVIEWS}>
+	//      {({ loading, error, data }) => {
+	//      if (loading) return "Loading...";
+	//      if (error) return `Error! ${error.message}`;
+	//      console.log(data);
 
-//      return (
+	//      return (
 
-//      <div>
+	//      <div>
 
-//      </div>
-//     );
-//      }}
-//     </Query>
-//    }) 
-//   </div>
-//  )}
+	//      </div>
+	//     );
+	//      }}
+	//     </Query>
+	//    })
+	//   </div>
+	//  )}
 
-render() {
+	render() {
+		const Reviews = ({ Review }) => (
+			<Query query={getReviews}>
+				{({ loading, error, data }) => {
+					if (loading) return <p>Loading...</p>;
+					if (error) return <p>Error :(</p>;
 
-const Reviews = ({Review}) => (
-  <Query 
-  query={getReviews}>
-  
- {({ loading, error, data }) => {
-     if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+					return (
+						<div className='card-container'>
+							{data.reviews.map(
+								({ id, text, name, timestamp, Author, ProjectReviewed }) => (
+									`${console.log(ProjectReviewed)}`,
+									(
+										
+											<div key={id} className='review-card'>
+												<p className='review-name'>Reviewer: {`${Author.username} `}</p>
+                        <h2 className='project-name'>Project: {`${ProjectReviewed.name}`}</h2>
+												<img className='review-img' src={`${ProjectReviewed.titleImg}`} />
 
-   return (
-    <div className="card-container">
-         {data.reviews.map(({ id, 
-         text, 
-         name, 
-         timestamp, 
-         Author, 
-         ProjectReviewed }) => (
-    `${console.log(ProjectReviewed)}`,
-           <div key={id}>
-           <div className="review-card">
-             <p className="review-name">
-             Reviewer: {`${ Author.username} `}
-              </p>
-              <h2 className="project-name">
-              Project: {`${ProjectReviewed.name}`}
-              </h2>
-              <img
-               className="review-img"
-               src={`${ProjectReviewed.titleImg}`}
-              />
+												<p className='review-text'>Text: {`${text}`}</p>
+												<p className='project-extra'>{`${name}`}</p>
+											</div>
+									
+									)
+								),
+							)}
+						</div>
+					);
+				}}
+			</Query>
+		);
 
-              <p className="review-text">Text: {`${text}`}</p>
-              <p className="project-extra">{`${name}`}</p>
-              </div>
-           </div>
-         ))}
-       </div>
-     );
-   }}
-
-
-
-
-
-
-  </Query>
-)
-
-return (
- <div>
-  <Reviews />
- </div>
-)
+		return (
+			<div>
+				<Reviews />
+			</div>
+		);
+	}
 }
 
-}
-
-export default ReviewModal
+export default ReviewModal;
