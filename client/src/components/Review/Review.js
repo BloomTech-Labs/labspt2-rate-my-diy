f2dd4b3ee68f0189b01b76076c335ef2977cc95b
Michael Trew <mtrew2015@gmail.com>
2019-04-11 00:37:29 -0400

    successfully pulling down review

--- client/src/components/Review/Review.js
+++ client/src/components/Review/Review.js
@@ -3,71 +3,60 @@ import { getReviews } from '../../query/query.js';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-var reviewArray = [];
+const GET_SPECIFIC_REVIEW = gql`
+	query review($id: ID!) {
+		review(where: { id: $id }) {
+			name
+			text
+			timestamp
+			ProjectReviewed {
+				name
+				category
+				titleImg
+			}
+		}
+	}
+`;
 class Review extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			reviews: reviewArray,
+			reviews: '',
 			review: '',
 			id: '',
 		};
 	}
 	reviewFinder = (id) => {
-    const reviews = this.state.reviews;
-  }
+		const reviews = this.state.reviews;
+		// console.log(reviews["0"].id, 'reviews inside function')
+		// const actualReview=reviews.filter(review => review.ProjectReviewed.id === id);
+		// console.log(actualReview, 'this is probably it')
+	};
 
 	componentDidMount() {
 		const id = this.props.match.params;
-    this.setState({id: id });
-		this.reviewFinder(id);
+		this.setState({ id: id });
+		this.reviewFinder(this.state.id);
 	}
 
 	render() {
+		const slicedArray = this.state.reviews.slice();
+		console.log(slicedArray['0'], 'first review in array ');
 		console.log(this.state.reviews, 'reviews array');
-		Review = ({ Review }) => (
-			<Query query={getReviews}>
+		const id = this.props.match.params;
+
+		Review = () => (
+			<Query query={GET_SPECIFIC_REVIEW} variables={id}>
 				{({ loading, error, data }) => {
 					if (loading) return <p>Loading...</p>;
 					if (error) return <p>Error :(</p>;
+					console.log('data from query', data);
 
-					var id = this.props.match.params;
-					var length = data.reviews.length,
-						i = 0;
-
-					for (let i = 0; i < data.reviews.length; i++) {
-						reviewArray.push(data.reviews[i]);
-					}
-
-					
-          console.log(this.state.review, 'review state');
-          console.log(this.state, 'entire state')
-					// const currentReview = data.reviews.find((review) => {
-					// 	return review.id === id;
-					// });
-					data.reviews.filter((review) => {
-						if (review.id === id) {
-							return review;
-						}
-					});
 					return (
 						<React.Fragment>
 							<h1 className='headerReview'>Reviews</h1>
-							<div className='card-container'>
-								{data.reviews.map(({ id, text, name, timestamp, Author, ProjectReviewed }) => (
-									<div key={id} className={'review-card'} to={`/reviews/${id}`}>
-										<div>
-											<p className='review-name'>Reviewer: {`${Author.username} `}</p>
-											<h2 className='project-name'>Project: {`${ProjectReviewed.name}`}</h2>
-											<img alt={'project'} className='review-img' src={`${ProjectReviewed.titleImg}`} />
-
-											<p className='review-text'>Text: {`${text}`}</p>
-											<p className='project-extra'>{`${name}`}</p>
-										</div>
-									</div>
-								))}
-							</div>
+							<div className='card-container' />
 						</React.Fragment>
 					);
 				}}
