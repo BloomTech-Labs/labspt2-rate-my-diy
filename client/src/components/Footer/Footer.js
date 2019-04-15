9a5bc3cd1f042fb096ddd881be90065f712651af
alasalle <angela.lasalle91@gmail.com>
2019-04-15 06:31:41 -0400

    added link to our Github project page in the footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,7 +7,9 @@ function Footer() {
     <div className="footer">
       <nav className="bottomNav">
         <Link to="/">Home</Link>
-        <Link to="/">About the Team</Link>
+        <Link to="https://github.com/Lambda-School-Labs/labspt2-rate-my-diy">
+          About the Team
+        </Link>
         <Link to="/subscribe">Support Us</Link>
       </nav>
       <p>
