f99dd37dade88404188742c33c2e3c7421ede418
alasalle <angela.lasalle91@gmail.com>
2019-04-18 00:29:14 -0400

    changed github sign in changed firebaseSignUp mutation to upsert the new/old user

--- client/src/components/SignIn/ThirdPartyApi/Github.js
+++ client/src/components/SignIn/ThirdPartyApi/Github.js
@@ -25,7 +25,9 @@ const CHECK_IF_USER_EXISTS = gql`
       thirdPartyUID
       username
       email
-    }`;
+    }
+  }
+`;
 
 const firebaseSignUp = gql`
   mutation firebaseSignUp(
@@ -76,22 +78,7 @@ class SignInGithubBase extends Component {
       .doSignInWithGithub()
       .then((socialAuthUser) => {
         const thirdPartyUID = socialAuthUser.user.providerData['0'].uid;
-        return (
-          <Query
-            query={CHECK_IF_USER_EXISTS}
-            variables={{ thirdPartyUID: thirdPartyUID }}
-          >
-            {({ loading, data, error }) => {
-              if (loading) return null;
-              if (error) {
-                console.log({ error: error });
-                return null;
-              }
-              if (data) console.log({ data: data });
-              return null;
-            }}
-          </Query>
-        );
+        this.setState({ uid: thirdPartyUID, isOpen: true });
       })
       .catch((err) => console.log(err));
   };
@@ -100,49 +87,99 @@ class SignInGithubBase extends Component {
     const { error } = this.state;
     return (
       <React.Fragment>
-        <form onSubmit={this.onSubmit}>
-          <button type="submit"> Sign In with Github </button>{' '}
-          {error && <p> {error.message} </p>}
-        </form>
-        <Modal isOpen={this.state.isOpen} contentLabel="Example Modal">
-          <div>
-            <h1>Complete Your Sign Up.</h1>
-            <Mutation mutation={firebaseSignUp}>
-              {(firebaseSignUp) => {
-                console.log({ state: this.state });
-                return (
-                  <form
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      firebaseSignUp({
-                        variables: {
-                          username: this.state.username,
-                          thirdPartyUID: this.state.uid,
-                          email: this.state.email
-                        }
-                      });
-                    }}
-                  >
-                    <input
-                      onChange={this.onChange}
-                      defaultValue={this.state.email}
-                      placeholder="email"
-                      name="email"
-                      value={this.state.email}
-                    />
-                    <input
-                      onChange={this.onChange}
-                      placeholder="username"
-                      name="username"
-                      value={this.state.username}
-                    />
-                    <button type="submit">Submit</button>
+        <Query
+          query={CHECK_IF_USER_EXISTS}
+          variables={{ thirdPartyUID: this.state.uid }}
+        >
+          {({ loading, data, error: checkError }) => {
+            if (loading)
+              return (
+                <form onSubmit={this.onSubmit}>
+                  <button type="submit" disabled>
+                    {' '}
+                    Sign In with Github{' '}
+                  </button>{' '}
+                  <div>Loading...</div>
+                  {error && <p> {error.message} </p>}
+                </form>
+              );
+            if (checkError) {
+              console.log({ error: checkError });
+              return (
+                <form onSubmit={this.onSubmit}>
+                  <button type="submit" disabled>
+                    {' '}
+                    Sign In with Github{' '}
+                  </button>{' '}
+                  <div>There was an error.</div>
+                  {error && <p> {error.message} </p>}
+                </form>
+              );
+            }
+            if (data && !data.user) {
+              return (
+                <div>
+                  <form onSubmit={this.onSubmit}>
+                    <button type="submit"> Sign In with Github </button>{' '}
+                    {error && <p> {error.message} </p>}
                   </form>
-                );
-              }}
-            </Mutation>
-          </div>
-        </Modal>
+                  <Modal
+                    isOpen={this.state.isOpen}
+                    contentLabel="Example Modal"
+                  >
+                    <div>
+                      <h1>Complete Your Sign Up.</h1>
+                      <Mutation mutation={firebaseSignUp}>
+                        {(firebaseSignUp) => {
+                          console.log({ state: this.state });
+                          return (
+                            <form
+                              onSubmit={(e) => {
+                                e.preventDefault();
+                                firebaseSignUp({
+                                  variables: {
+                                    username: this.state.username,
+                                    thirdPartyUID: this.state.uid,
+                                    email: this.state.email
+                                  }
+                                });
+                              }}
+                            >
+                              <input
+                                onChange={this.onChange}
+                                defaultValue={this.state.email}
+                                placeholder="email"
+                                name="email"
+                                value={this.state.email}
+                              />
+                              <input
+                                onChange={this.onChange}
+                                placeholder="username"
+                                name="username"
+                                value={this.state.username}
+                              />
+                              <button type="submit">Submit</button>
+                            </form>
+                          );
+                        }}
+                      </Mutation>
+                    </div>
+                  </Modal>
+                </div>
+              );
+            }
+            if (data && data.user.thirdPartyUID) {
+              this.props.history.push(ROUTES.HOME);
+            }
+            return (
+              <form onSubmit={this.onSubmit}>
+                <button type="submit"> Sign In with Github </button>{' '}
+                <div>Loading...</div>
+                {error && <p> {error.message} </p>}
+              </form>
+            );
+          }}
+        </Query>
       </React.Fragment>
     );
   }
