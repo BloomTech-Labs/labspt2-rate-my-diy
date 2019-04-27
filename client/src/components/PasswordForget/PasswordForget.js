95c575798e755a3523996445fdf71558927e2314
Michael Trew <mtrew2015@gmail.com>
2019-04-27 13:52:39 -0400

    style password reset view

--- client/src/components/PasswordForget/PasswordForget.js
+++ client/src/components/PasswordForget/PasswordForget.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withFirebase } from '../Firebase/Exports';
 import * as ROUTES from '../../constants/routes';
+import './PasswordForget.scss';
 
 const PasswordForgetPage = () => (
-  <div>
-    <h1>Password Forget</h1>
+  <div className="passwordContainer">
+    <h1>Reset Your Password</h1>
+    <p>Just enter your email and we will send you a new password.</p>
     <PasswordForgetForm />
   </div>
 );
@@ -39,19 +41,21 @@ class PasswordForgetBase extends Component {
     const { email, error } = this.state;
     const isInvalid = email === '';
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={this.state.email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <button disabled={isInvalid} type="submit">
-          Reset My Password
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
+      <div className="forgotPasswordForm">
+        <form onSubmit={this.onSubmit}>
+          <input
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"
+          />
+          <button disabled={isInvalid} type="submit">
+            Reset My Password
+          </button>
+          {error && <p>{error.message}</p>}
+        </form>
+      </div>
     );
   }
 }
