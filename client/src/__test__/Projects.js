4fe9306b73c0e9268f29813c7a04c1d4c75155a6
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-07 23:23:43 -0400

    Working on project testing.

--- client/src/__test__/Projects.js
+++ client/src/__test__/Projects.js
@@ -4,4 +4,12 @@ import gql from 'graphql-tag';
 import { getProjects as GET_PROJECTS } from '../test-queries/test-queries';
 import { projectMocks } from './data-mocks/data-mocks';
 
-export default () => <Query query={GET_PROJECTS} />;
+export default () => (
+  <Query query={GET_PROJECTS}>
+    {({ loading, data, error }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error!</p>;
+    }}
+    ) ); }}
+  </Query>
+);
