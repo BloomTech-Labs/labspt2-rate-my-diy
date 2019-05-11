95b1b40435f034f127e7972f6d5cd012c5766882
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-11 14:34:06 -0400

    Adding mock schema to test files.

--- server/__tests__/assertions.test.js
+++ server/__tests__/assertions.test.js
@@ -4,49 +4,39 @@ const fs = require('fs');
 const path = require('path');
 const { expect } = require('chai');
 const schema = fs.readFileSync(
-  path.join(__dirname, '..', '.schema.graphql'),
+  path.join(__dirname, '..', '.../../../schema.graphql'),
   'utf8'
 );
 
 describe('Testing schema, Query', () => {
-  tester: beforeAll(() => {
-    tester: new EGQLT(schema);
+  let tester;
+  beforeAll(() => {
+    tester = new EGQLT(schema);
   });
-
-  describe('Queries', () => {
-    test('Should pass with multiple queries', () => {
-      const userQuery = `
-    $id: ID!
-    $username: String!
-    $bio: String!
-    $email: String!
-    $account: String @default(value: 'free-tier)
-   `;
-      tester.test(true, userQuery, {
-        id: 'cjunieium00u50765xx6b78i5'
-      });
-    });
-
-    test('Should return an object.', () => {
-      let error;
-      try {
-        const query = `
-    $id: ID!
-    $username: String!
-    $bio: String!
-    $email: String!
-    $account: String @default(value: 'free-tier)
-   `;
-        tester.mock({
-          query,
-          variables: {
-            id: 'cjunieium00u50765xx6b78i5'
-          }
-        });
-      } catch (err) {
-        error = err;
+  describe('Should pass if the query is invalid', () => {
+    it('Invalid query getUsers', () => {
+      const invalidQuery = `
+     {
+      getUsers {
+       id
+       thirdPartyUID
+       firebaseUID
+       username
+       email
+       invalidField
+       userProfileImage
+       bio
+       privilege
+       stripeId
+       accountType
+       ReviewList
+       LikedReviews
+       DislikedReviews
+       Projects
+       RatedProjects
       }
-      expect(error).to.exist;
+     }
+    `;
     });
   });
 });
