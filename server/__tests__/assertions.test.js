0e000c820bb3336fb7a9cc5a2825c2dc8779dc74
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-11 13:12:47 -0400

    Implementing graphql tester.

---
+++ server/__tests__/assertions.test.js
@@ -0,0 +1,9 @@
+'use strict';
+const EGQLT = require('easygraphql-tester');
+const fs = require('fs');
+const path = require('path');
+const { expect } = require('chai');
+const schema = fs.readFileSync(
+  path.join(__dirname, '..', '.schema.graphql'),
+  'utf8'
+);
