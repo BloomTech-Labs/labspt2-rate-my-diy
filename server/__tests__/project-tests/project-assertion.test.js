0f9dfa19cc6e4c0986b676f60a13e823dc196625
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-12 17:41:30 -0400

    Testing root level/nested project queries.

--- server/__tests__/project-tests/project-assertion.test.js
+++ server/__tests__/project-tests/project-assertion.test.js
@@ -57,5 +57,151 @@ describe('Testing schema (Project), Query', () => {
     });
   });
 
-  describe();
+  describe('Should pass if the nested project query is valid.', () => {
+    it('Is a valid nested project query.', () => {
+      const valiedNestedProjectQuery = `
+    {
+     projects {
+      id 
+      name
+      key
+      category
+      timestamp
+      titleImg
+      titleBlurb
+      rating
+      steps
+      User {
+       id
+     username
+     userProfileImage
+     bio
+     email
+      }
+      Reviews {
+       id
+       name
+       rKey
+       text
+       timestamp
+       thumbsUp
+       LikedBy {
+        id
+        email
+        username
+        userProfileImage
+        bio
+        privilege
+        accountType
+       }
+       DislikedBy {
+        id
+        email
+        username
+        bio
+       }
+       Author {
+        username
+        email
+        bio
+        id
+       }
+       ProjectReviewed {
+        id
+        name
+        key
+        category
+        timestamp
+        titleImg
+        titleBlurb
+        rating
+        steps
+        User {
+         username
+         email
+         id
+        }
+       }
+       thumbsDown
+      }
+     }
+    }
+    `;
+      tester.test(true, valiedNestedProjectQuery);
+    });
+  });
+  describe('Should pass iff the nested project query is invalid.', () => {
+    it('Is a valid nested project query.', () => {
+      const valiedNestedProjectQuery = `
+    {
+     projects {
+      id 
+      name
+      key
+      category
+      timestamp
+      titleImg
+      titleBlurb
+      rating
+      invalidField
+      steps
+      User {
+       id
+     username
+     userProfileImage
+     bio
+     email
+      }
+      Reviews {
+       id
+       name
+       rKey
+       text
+       timestamp
+       thumbsUp
+       LikedBy {
+        id
+        email
+        username
+        userProfileImage
+        bio
+        privilege
+        accountType
+       }
+       DislikedBy {
+        id
+        email
+        username
+        bio
+       }
+       Author {
+        username
+        email
+        bio
+        id
+       }
+       ProjectReviewed {
+        id
+        name
+        key
+        category
+        timestamp
+        titleImg
+        titleBlurb
+        rating
+        steps
+        User {
+         username
+         email
+         id
+        }
+       }
+       thumbsDown
+      }
+     }
+    }
+    `;
+      tester.test(false, valiedNestedProjectQuery);
+    });
+  });
 });
